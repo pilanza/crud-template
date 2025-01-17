6c87282392199ff046650ce37ff10cedf05b1d71
Pietro Lanza <pietrolp333@gmail.com>
2025-01-16 23:49:49 -0300

    :lipstick: fix: fixed layout styles

--- src/app/(dashboard)/admin/layout.tsx
+++ src/app/(dashboard)/admin/layout.tsx
@@ -1,7 +1,4 @@
 import { Sidebar } from "@/components/dashboardComponents/sidebar/sidebar";
-import { Inter } from "next/font/google"
-
-const inter = Inter({subsets: ["latin"]});
 
 export const apiUrl = "http://localhost:3000/api"
 
@@ -11,7 +8,7 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-      <div className={`${inter.className} text-stone-950 bg-stone-100 grid gap-4 m-4 grid-cols-[220px,_1fr]`}>
+      <div className={`grid gap-4 m-4 grid-cols-[220px,_1fr]`}>
         <Sidebar />
         <div className='bg-white rounded-lg pb-4 shadow h-[200vh]'>
           {children}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
 import { Providers } from '../providers/providers'
+import { Inter } from "next/font/google"
+
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
   title: 'Next.js',
@@ -13,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body>
+      <body className={`${inter.className} text-stone-950 bg-stone-100`}>
         <Providers>
           {children}
         </Providers>
